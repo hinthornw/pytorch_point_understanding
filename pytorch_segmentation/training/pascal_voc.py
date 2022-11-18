051b9dc19aee15f2d5b663c99b62dd1a6030534b
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-18 10:09:55 +0000

    Adding tarfile member sanitization to extractall()

--- pytorch_segmentation/training/pascal_voc.py
+++ pytorch_segmentation/training/pascal_voc.py
@@ -156,7 +156,26 @@ class PascalVOCSegmentation(data.Dataset):
     def _extract_tar_to_the_root_folder(self, tar_full_filename):
         # TODO: change to with: statement instead
         with tarfile.open(tar_full_filename) as tf:
-          tf.extractall(path=self.root)
+          def is_within_directory(directory, target):
+              
+              abs_directory = os.path.abspath(directory)
+              abs_target = os.path.abspath(target)
+          
+              prefix = os.path.commonprefix([abs_directory, abs_target])
+              
+              return prefix == abs_directory
+          
+          def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+          
+              for member in tar.getmembers():
+                  member_path = os.path.join(path, member.name)
+                  if not is_within_directory(path, member_path):
+                      raise Exception("Attempted Path Traversal in Tar File")
+          
+              tar.extractall(path, members, numeric_owner=numeric_owner) 
+              
+          
+          safe_extract(tf, path=self.root)
         # tar_obj = tarfile.open(tar_full_filename)
         # 
         # tar_obj.extractall(path=self.root)
